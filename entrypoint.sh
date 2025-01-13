d9af6a107c7ac4b7c8f6385162da01f9ec6d4853
Pooya Sharifi <pooyasharifi.sp2@gmail.com>
2025-01-14 00:35:22 +0330

    feat: Add topic input to action.yml and include in message payload

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,17 @@
 
 token_id=$1
 chat_id=$2
-text=$3
-parse_mode=$4
-disable_web_page_preview=$5
-disable_notification=$6
+topic=$3
+text=$4
+parse_mode=$5
+disable_web_page_preview=$6
+disable_notification=$7
 
-response=`curl -X POST https://api.telegram.org/bot$token_id/sendMessage -H 'Content-Type: application/json' -d "{\"chat_id\": \"$chat_id\", \"text\": \"$text\", \"parse_mode\": \"$parse_mode\", \"disable_web_page_preview\": \"$disable_web_page_preview\", \"disable_notification\": \"$disable_notification\"}"}`
+# Include the topic in the message
+message="Topic: $topic\n$text"
+
+response=$(curl -X POST "https://api.telegram.org/bot$token_id/sendMessage" \
+-H 'Content-Type: application/json' \
+-d "{\"chat_id\": \"$chat_id\", \"text\": \"$message\", \"parse_mode\": \"$parse_mode\", \"disable_web_page_preview\": \"$disable_web_page_preview\", \"disable_notification\": \"$disable_notification\"}")
 
 echo "response=$response" >> $GITHUB_OUTPUT
