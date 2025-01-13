81a1e5de02694aba39de5b39bbf144a5a22c5e46
Pooya Sharifi <pooyasharifi.sp2@gmail.com>
2025-01-14 00:48:26 +0330

    fix: Update message formatting in entrypoint.sh to include message_thread_id

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ disable_web_page_preview=$6
 disable_notification=$7
 
 # Include the topic in the message
-message="Topic: $topic\n$text"
 
 response=$(curl -X POST "https://api.telegram.org/bot$token_id/sendMessage" \
 -H 'Content-Type: application/json' \
--d "{\"chat_id\": \"$chat_id\", \"text\": \"$message\", \"parse_mode\": \"$parse_mode\", \"disable_web_page_preview\": \"$disable_web_page_preview\", \"disable_notification\": \"$disable_notification\"}")
+-d "{\"chat_id\": \"$chat_id\",\"message_thread_id\":\"$topic\" \"text\": \"$message\", \"parse_mode\": \"$parse_mode\", \"disable_web_page_preview\": \"$disable_web_page_preview\", \"disable_notification\": \"$disable_notification\"}")
+
 
 echo "response=$response" >> $GITHUB_OUTPUT
